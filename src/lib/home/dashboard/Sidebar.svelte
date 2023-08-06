8a39f94ec28db7f56c78478fdc3def5c428f4973
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-08-06 23:07:41 +0530

    Updated Nav

--- src/lib/home/dashboard/Sidebar.svelte
+++ src/lib/home/dashboard/Sidebar.svelte
@@ -22,8 +22,8 @@
 -->
 <script>
 	import { page } from '$app/stores';
-	import { TrendingUp, Home } from 'lucide-svelte';
-	let isMenuOpen = false;
+	import { Pen } from 'lucide-svelte';
+	let isMenuOpen = true;
 	let isProfileOpen = false;
 	let profileNavs = [
 		{
@@ -63,20 +63,22 @@
           From: "opacity-100"
           To: "opacity-0"
       -->
-		<div class="fixed inset-0 bg-gray-900/80" />
+		<div
+			class=" {isMenuOpen
+				? 'transition-opacity ease-linear duration-300 opacity-0 '
+				: 'opacity-100 transition-opacity ease-linear duration-300 fixed inset-0 '} fixed top-0  -z-10 bg-gray-900/80"
+		/>
 
-		<div class="fixed inset-0 flex">
-			<!--
-          Off-canvas menu, show/hide based on off-canvas menu state.
-  
-          Entering: "transition ease-in-out duration-300 transform"
-            From: "-translate-x-full"
-            To: "translate-x-0"
-          Leaving: "transition ease-in-out duration-300 transform"
-            From: "translate-x-0"
-            To: "-translate-x-full"
-        -->
-			<div class="relative mr-16 flex w-full max-w-xs flex-1">
+		<div
+			class="{isMenuOpen
+				? ' opacity-0 hidden'
+				: ' opacity-100 fixed inset-0'}  fixed top-0 flex -z-10"
+		>
+			<div
+				class="{isMenuOpen
+					? 'transition ease-in-out duration-300 transform -translate-x-full'
+					: 'transition ease-in-out duration-300 transform translate-x-0'} relative mr-16 flex w-full max-w-xs flex-1"
+			>
 				<!--
             Close button, show/hide based on off-canvas menu state.
   
@@ -87,8 +89,19 @@
               From: "opacity-100"
               To: "opacity-0"
           -->
-				<div class="absolute left-full top-0 flex w-16 justify-center pt-5">
-					<button type="button" class="-m-2.5 p-2.5">
+				<div
+					class="{isMenuOpen
+						? 'ease-in-out duration-300 opacity-0 '
+						: ' opacity-100 ease-in-out duration-300'} absolute left-full top-0 flex w-16 justify-center pt-5"
+				>
+					<button
+						type="button"
+						class="-m-2.5 p-2.5 border-none outline-none"
+						on:click={() => {
+							isMenuOpen = !isMenuOpen;
+							console.log('Clicked');
+						}}
+					>
 						<span class="sr-only">Close sidebar</span>
 						<svg
 							class="h-6 w-6 text-white"
@@ -105,19 +118,22 @@
 
 				<!-- Sidebar component, swap this element with another sidebar if you like -->
 				<div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
-					<div class="flex h-16 shrink-0 items-center">
+					<div
+						class="flex h-16 shrink-0 items-center gap-3 bg-gray-200/80 justify-center rounded-xl mt-2"
+					>
 						<img
-							class="h-8 w-auto"
-							src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+							class="h-10 w-auto rounded-full"
+							src="https://i.pinimg.com/564x/f1/b9/49/f1b949ff0694ee4d2c7c219c5237d6be.jpg"
 							alt="Your Company"
 						/>
+						<h1 class="text-lg font-mono">Write Your Today</h1>
 					</div>
 					<nav class="flex flex-1 flex-col">
 						<ul role="list" class="flex flex-1 flex-col gap-y-7">
 							<li>
 								<ul role="list" class="-mx-2 space-y-1">
-									<li>
-										<!-- Current: "bg-gray-50 text-indigo-600", Default: "text-gray-700 hover:text-indigo-600 hover:bg-gray-50" -->
+									<!-- <li>
+										Current: "bg-gray-50 text-indigo-600", Default: "text-gray-700 hover:text-indigo-600 hover:bg-gray-50"
 										<a
 											href="#"
 											class="bg-gray-50 text-indigo-600 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
@@ -160,104 +176,25 @@
 											</svg>
 											Team
 										</a>
-									</li>
-									<li>
-										<a
-											href="#"
-											class="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-										>
-											<svg
-												class="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
-												fill="none"
-												viewBox="0 0 24 24"
-												stroke-width="1.5"
-												stroke="currentColor"
-												aria-hidden="true"
+									</li> -->
+									{#each sideNavs as item}
+										<li>
+											<a
+												on:click={() => (isMenuOpen = true)}
+												href={item.href}
+												class=" {isActive === item.href
+													? 'text-blue-500 '
+													: 'text-gray-500'} hover:text-blue-500 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
 											>
-												<path
-													stroke-linecap="round"
-													stroke-linejoin="round"
-													d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
-												/>
-											</svg>
-											Projects
-										</a>
-									</li>
-									<li>
-										<a
-											href="#"
-											class="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-										>
-											<svg
-												class="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
-												fill="none"
-												viewBox="0 0 24 24"
-												stroke-width="1.5"
-												stroke="currentColor"
-												aria-hidden="true"
-											>
-												<path
-													stroke-linecap="round"
-													stroke-linejoin="round"
-													d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-												/>
-											</svg>
-											Calendar
-										</a>
-									</li>
-									<li>
-										<a
-											href="#"
-											class="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-										>
-											<svg
-												class="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
-												fill="none"
-												viewBox="0 0 24 24"
-												stroke-width="1.5"
-												stroke="currentColor"
-												aria-hidden="true"
-											>
-												<path
-													stroke-linecap="round"
-													stroke-linejoin="round"
-													d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
-												/>
-											</svg>
-											Documents
-										</a>
-									</li>
-									<li>
-										<a
-											href="#"
-											class="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-										>
-											<svg
-												class="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
-												fill="none"
-												viewBox="0 0 24 24"
-												stroke-width="1.5"
-												stroke="currentColor"
-												aria-hidden="true"
-											>
-												<path
-													stroke-linecap="round"
-													stroke-linejoin="round"
-													d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
-												/>
-												<path
-													stroke-linecap="round"
-													stroke-linejoin="round"
-													d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
-												/>
-											</svg>
-											Reports
-										</a>
-									</li>
+												{@html item.icon}
+												{item.name}
+											</a>
+										</li>
+									{/each}
 								</ul>
 							</li>
 							<li>
-								<div class="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
+								<div class="text-xs font-semibold leading-6 text-gray-400">Following</div>
 								<ul role="list" class="-mx-2 mt-2 space-y-1">
 									<li>
 										<!-- Current: "bg-gray-50 text-indigo-600", Default: "text-gray-700 hover:text-indigo-600 hover:bg-gray-50" -->
@@ -269,7 +206,7 @@
 												class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600"
 												>H</span
 											>
-											<span class="truncate">Heroicons</span>
+											<span class="truncate">Svelte</span>
 										</a>
 									</li>
 									<li>
@@ -281,7 +218,7 @@
 												class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600"
 												>T</span
 											>
-											<span class="truncate">Tailwind Labs</span>
+											<span class="truncate">SvelteKit</span>
 										</a>
 									</li>
 									<li>
@@ -293,36 +230,19 @@
 												class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600"
 												>W</span
 											>
-											<span class="truncate">Workcation</span>
+											<span class="truncate">Supabase</span>
 										</a>
 									</li>
 								</ul>
 							</li>
 							<li class="mt-auto">
 								<a
-									href="#"
-									class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600"
+									href="/app/create"
+									class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-white bg-gray-900 items-center justify-center mx-2"
+									on:click={() => (isMenuOpen = true)}
 								>
-									<svg
-										class="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke-width="1.5"
-										stroke="currentColor"
-										aria-hidden="true"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-										/>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-										/>
-									</svg>
-									Settings
+									<Pen strokeWidth="1.5px" size="18" />
+									Create Post
 								</a>
 							</li>
 						</ul>
@@ -484,7 +404,11 @@
 		<div
 			class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8"
 		>
-			<button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden">
+			<button
+				type="button"
+				class="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+				on:click={() => (isMenuOpen = !isMenuOpen)}
+			>
 				<span class="sr-only">Open sidebar</span>
 				<svg
 					class="h-6 w-6"
@@ -505,8 +429,8 @@
 			<!-- Separator -->
 			<div class="h-6 w-px bg-gray-200 lg:hidden" aria-hidden="true" />
 
-			<div class="flex flex-1 gap-x-4 self-stretch lg:gap-x-6 ">
-				<form class="relative flex flex-1 " action="#" method="GET">
+			<div class="flex flex-1 gap-x-4 self-stretch lg:gap-x-6 z-50">
+				<form class="relative flex flex-1" action="#" method="GET">
 					<label for="search-field" class="sr-only">Search</label>
 					<svg
 						class="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
@@ -554,11 +478,14 @@
 					<div class="relative">
 						<button
 							type="button"
-							class="-m-1.5 flex items-center p-1.5 outline-none border-none"
+							class="-m-1.5 flex items-center p-1.5 outline-none border-none group z-50"
 							id="user-menu-button"
 							aria-expanded="false"
 							aria-haspopup="true"
-							on:click={() => (isProfileOpen = !isProfileOpen)}
+							on:click={() => {
+								isProfileOpen = !isProfileOpen;
+								console.log('Clickeddd');
+							}}
 						>
 							<span class="sr-only">Open user menu</span>
 							<img
@@ -568,12 +495,12 @@
 							/>
 							<span class="hidden lg:flex lg:items-center">
 								<span
-									class="ml-4 text-sm font-semibold leading-6 text-gray-600 font-mono md:text-[18px] md:font-light hover:text-gray-900"
+									class="ml-4 text-sm font-semibold leading-6 text-gray-700 font-mono md:text-[18px] md:font-light group-hover:text-gray-900"
 									aria-hidden="true"
 									>Lofii
 								</span>
 								<svg
-									class="ml-2 h-5 w-5 text-gray-400"
+									class="ml-2 h-5 w-5 text-gray-400 group-hover:text-gray-700"
 									viewBox="0 0 20 20"
 									fill="currentColor"
 									aria-hidden="true"
